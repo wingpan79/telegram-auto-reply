3eba4c3db8aa7b4e04a1259cc3b10973eab5bddf
wingpan79 <wingpan79@gmail.com>
2021-07-12 12:29:50 +0800

    Update auto-reply.py
    ignore bot msg

--- auto-reply.py
+++ auto-reply.py
@@ -7,23 +7,29 @@ from telethon.tl.types import InputStickerSetID
 
 trigger = "揼"
 reply = ["邊位"]
-group_id = [-1001210202420, -1001425821503] #group id list
+group_id = [-1001210202420, -1001425821503]  # group id list
 if __name__ == '__main__':
     # Create the client and connect
     # use sequential_updates=True to respond to messages one at a time
     client = TelegramClient(name, api_id, api_hash)
+
     @client.on(events.NewMessage(incoming=True))
     async def handle_new_message(event):
-      if event.is_private: 
-           await event.reply(random.choice(reply)) #reply in private
-      if event.is_group: #reply in group
-        #print(event,event.chat_id)
-        if event.chat_id in group_id:  
-         from_ = await event.client.get_entity(event.from_id)  # this lookup will be cached by telethon
-         print(from_.first_name,from_.last_name,event.message.text,event.from_id)
-         text = str(event.message.message)
-         if text.find(trigger) != -1:
-            await event.reply(random.choice(reply)) #if contain word then reply
+        if event.is_private:
+            await event.reply(random.choice(reply))  # reply in private
+        if event.is_group:  # reply in group
+            # print(event,event.chat_id)
+            if event.chat_id in group_id:
+                # this lookup will be cached by telethon
+                from_ = await event.client.get_entity(event.from_id)
+                if from_.bot == False: #ignore bot msg
+                    return
+                print(from_.first_name, from_.last_name,
+                      event.message.text, event.from_id)
+                text = str(event.message.message)
+                if text.find(trigger) != -1:
+                    # if contain word then reply
+                    await event.reply(random.choice(reply))
     print(time.asctime(), '-', 'Auto-replying...')
     client.start()
     client.run_until_disconnected()
